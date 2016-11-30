18270cf4be703fb1c41f95b00e7c92cb7bded5fe
Gisellabella <gisella@gmail.com>
2016-11-29 21:34:04 -0700

    wee bit stuck

--- app.js
+++ app.js
@@ -11,18 +11,24 @@ mongo.connect(url, function(err, db) {
         collection.find ({"name": specificRestaurant}).toArray(function(err, doc) {
         console.log(doc); 
         });
-    }   else {
-        console.log ("Hmm, we don't recognize that restaurant name, please try again.");
+    }  
+    /* Task 2: Create a prompt for users to add their own restaurants.*/
+      else {
+        console.log ("Hmm, we don't recognize that restaurant name, would you like to add it?");
+        var newName = prompt("Restaurant Name");
+        var streetAdress = prompt("Street Address");//can we combine prompts,also offer a "i dont know option"
+        var end = prompt("I don't know or cancel button");
+        (addRestaurant = "new" ) {
+            collection.insert({
+            "name": newName,
+            "address" : {
+                        street : streetAdress,
+                        zipcode : 20001,
+                        },
+            yelp: "http://www.yelp.com/biz/cookies-corner-washington"
+            });
+         console.log ();
+        }
         }   
 });
 
-
-/* var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
-  if(allChoice == "all"){
-    collection.find().toArray(function(err, doc){
-      console.log(doc);
-    });
-  } else {
-    console.log("Aw, you don't want to see the restaurants?");
-  }
-}); */
