becfed1585948e53d75c239c9d56f2382be04714
Gisellabella <gisella@gmail.com>
2016-12-07 00:01:02 -0700

    I started over..it seems as if it shoudl work. what am i missing?

--- app.js
+++ app.js
@@ -2,32 +2,29 @@ var mongo = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/restaurant_db";
 var prompt = require("prompt-sync")();
 
-mongo.connect(url, function(err, db) {
-  var collection = db.collection('restaurants');
-  /* Task 1: Comment out the first prompt and add another prompt
-  to let the user view information about a specific restaurant, by entering a name.  */
-  var specificRestaurant = prompt("Please, enter the name of the restaurant you are looking for...");{ 
-    if (specificRestaurant == "name")
-    collection.find({"name": specificRestaurant}).toArray(function(err, doc){
-      console.log(doc);
-      }); else 
-      {
-          /* Task 2: Create a prompt for users to add their own restaurants.*/
-          console.log ("Hmm, we don't recognize that restaurant name, would you like to add it?");
-          var newName = prompt("Restaurant Name?");
-          var newStreet = prompt("Street Address?");
-          var newZip = prompt("Zip Code?");//can we combine prompts and also offer a "i dont know option"?
-          collection.insertOne({
-                                "name": newName,
-                                "address" : {
-                                "street": newStreet,
-                                "zipcode" : newZip,
-                                            },
-                                });
-            collection.find().toArray(function(err, doc){
-            console.log(doc);
-            });
-        }
-    }
-  }
-);
+mongo.connect(url, function(err, db){
+    var collection = db.collection('restaurants');
+    var specificRestaurant = prompt("Please, enter the name of the restaurant you are looking for...");
+    if (specificRestaurant){
+      collection.find({name: specificRestaurant}).forEach(function(item){
+        var result = [];
+        console.log(result);
+        result.push(item);
+      
+      });
+    } else  {
+    /* Task 2: Create a prompt for users to add their own restaurants.*/
+     console.log ("Hmm, we don't recognize that restaurant name, would you like to add it?");
+    var newName = prompt("Restaurant Name?");
+    var newStreet = prompt("Street Address?");
+    var newZip = prompt("Zip Code?");
+    collection.insert ({
+                            "name": newName,
+                         "address" : {
+                                    "street": newStreet,
+                                         "zipcode" : newZip,
+                                        },
+   });    
+ 
+}
+});
